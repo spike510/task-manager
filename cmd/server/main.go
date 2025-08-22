a8ad72f27b375c28afe4363a7dfb896982ccc4a1
pnowok <pnowok@gmail.com>
2025-08-22 23:08:35 +0200

    Migrations for users and tasks tables

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,10 +3,33 @@ package main
 import (
 	"log"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+	"github.com/spike510/task-manager/internal/db"
 	"github.com/spike510/task-manager/internal/http"
 )
 
 func main() {
+	// Database connection
+	database, err := db.Connect()
+	if err != nil {
+		log.Fatal("DB connection failed:", err)
+	}
+	defer database.Close()
+
+	// Run migrations
+	driver, err := postgres.WithInstance(database, &postgres.Config{})
+	m, err := migrate.NewWithDatabaseInstance(
+		"file:///app/migrations",
+		"postgres",
+		driver,
+	)
+	m.Up()
+
+	log.Println("migrations applied successfully")
+
 	r := http.NewRouter()
 
 	log.Println("Server running on :8080")
